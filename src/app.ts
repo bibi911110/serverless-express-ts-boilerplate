de53a609a934a362e792478cace0dd194d3f458e
bibi911110 <happidev54524@gmail.com>
2024-04-05 00:38:48 +0900

    remove unneccessary part

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes';
 import { errorHandler } from './utils/errorHandler';
-import db from './config/seqeulize';
 
 const app = express();
 
